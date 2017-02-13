df23122b05ccd943a31be0c9083cf37709fd77bd
Eugene <kudriashov.e@gmail.com>
2017-02-13 09:21:28 +0200

    footer markup

--- footer.php
+++ footer.php
@@ -4,3 +4,34 @@
  * @package ng-WordPress
  */
 ?>
+
+<!-- SECTION: Footer -->
+	<footer class="has-padding footer-bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-5 footer-branding">
+					<!-- <img class="footer-branding-logo" src="img/logo1.png" alt=""> -->
+					<p>AngularJS powered Wordpress Theme by <a href="#">Eugene Kudriashov</a> <br/> exclusively for <span class="bold-italic">Fun</span></p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 footer-nav">
+					<ul class="footer-primary-nav">
+						<li><a href="">link1</a></li>
+						<li><a href="">link2</a></li>
+						<li><a href="">link3</a></li>
+					</ul>
+					<ul class="footer-secondary-nav">
+						<li>&copy; nGWp 2017. AngularJS powered Wordpress Theme by <a href="">Eugene Kudriashov</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- END SECTION: Footer -->
+
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
